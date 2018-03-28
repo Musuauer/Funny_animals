88fa1375273e59c814ef6819822b14c509995e28
guillermo gudiño romo <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-03-28 17:43:25 +0200

    first try at shuffling

--- js/app.js
+++ js/app.js
@@ -1,18 +1,11 @@
 /*
  * Create a list that holds all of your cards
  */
+const allCards = document.querySelectorAll('i.fa');
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -23,7 +16,12 @@ function shuffle(array) {
     }
 
     return array;
+ 
 }
+let newCards = shuffle(allCards);
+
+// allCards.parentNode.replaceChild(newCards,allCards);
+
 
 
 /*
