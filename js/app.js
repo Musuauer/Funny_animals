e1e7f2f876f507333b5774e5daa0ff4a41f4b708
guillermo gudiño romo <guillermogudinoromo@guillermos-MBP.fritz.box>
2018-04-09 22:46:05 +0200

    improve order and create sections of comments

--- js/app.js
+++ js/app.js
@@ -1,49 +1,50 @@
+//--------------------general variables
+const cards = deck.children;
+let openCards = [];
+let matchedCards = [];
+
+// get all cards  in a list
 const allCards = document.querySelectorAll('.card');
 const oldCards = [...allCards].map(i => i.outerHTML);
-
+//shuffle cards
 let newCards = shuffle(oldCards);
-
+//add shuffled cards to HTML
 let deck = document.querySelector('.deck');
 deck.innerHTML = newCards.join('');
 
-const cards = deck.children;
 
-let openCards = [];
+//------------------------score-panel
+//---stars
+const stars = document.querySelector('.stars').children;
+let starArray = [...stars];
+//---restart
+const restartButton = document.querySelector('.restart');
+restartButton.addEventListener('click', restartGame);
+//---moves
+let moves = 0;
+const counter = document.querySelector('.moves');
+//---timer
+let sec = 0;
+let timer = 0;
+let seconds = document.getElementById('seconds');
+let minutes = document.getElementById('minutes');
 
-let matchedCards = [];
 
+//---------------------------modal
 const congrats = document.querySelector('.popup');
-
+//---restart-from-modal
 const closeButton = document.querySelector('.close-button');
 closeButton.addEventListener('click', restartGame);
-
+//---stars, moves, timer
 const starsPopup = document.getElementById('stars-result');
 const movesPopup = document.getElementById('moves-result');
 const minutesPopup = document.getElementById('minutes-result');
 const secondsPopup = document.getElementById('seconds-result');
 
-const stars = document.querySelector('.stars').children;
-let starArray = [...stars];
-
-const restartButton = document.querySelector('.restart');
-restartButton.addEventListener('click', restartGame);
-
-let moves = 0;
-const counter = document.querySelector('.moves');
-
-let sec = 0;
-let seconds = document.getElementById('seconds');
-let minutes = document.getElementById('minutes');
-
-let timer = 0;
 
 
 
-
-
-
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+// --------------Shuffle function from http://stackoverflow.com/a/2450976
 
 function shuffle(array) {
     let currentIndex = array.length,
@@ -60,7 +61,7 @@ function shuffle(array) {
     return array;
 
 }
-
+// -------------------------------------------MODAL-------------------------------------------//
 // Function to feed modal with results
 
 function toggleModal(moves, starArray) {
@@ -83,93 +84,16 @@ function toggleModal(moves, starArray) {
     congrats.classList.toggle('show-popup');
 }
 
-// If all the cards are matched, call the modal
-function won(matchedCards) {
-    if (matchedCards.length === 16) {
-        clearInterval(timer);
-        toggleModal(moves,starArray);
-}
-}
 
-// add classes to matched cards
-function addMatchClass(card1, card2){
-    card1.children[0].classList.add('paired');
-    card2.children[0].classList.add('paired');
-    card1.classList.add('match');
-    card2.classList.add('match');
 
-}
-
-// remove and hide cards
-
-function removeAndHide(card1, card2){
-    card1.classList.remove('open');
-    card2.classList.remove('open');
-    openCards = [];
-}
-
-// add pairs to matched array and call function to add classes
-
-function addToMatched(matchedCards, card1,card2) {
-    matchedCards.push(card1);
-    matchedCards.push(card2);
-    addMatchClass(card1, card2);
-
-    won(matchedCards);
-}
-
- // Timer function adapted from https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
-
-//  function stopTimer(timer) {
-//     clearInterval(timer);
-//   }
-
-function pad (val) { 
-    return val > 9 ? val : "0" + val; 
-}
- function timerFunction(timer){
-    seconds.innerHTML = pad(++sec % 60);
-    minutes.innerHTML = pad(parseInt(sec / 60, 10));
-}
-
-// Function to compare opened cards
-
-function compareCards(openCards) {
-
-    const card1 = openCards[0];
-    const card2 = openCards[1];
-
-    if (openCards.length === 2){
-        if(card1.innerHTML === card2.innerHTML){
-            addToMatched(matchedCards, card1, card2);
-            removeAndHide(card1, card2);
-
-        }
-
-        else {
-            setTimeout(
-                () => removeAndHide(card1, card2),
-                1200
-            );
-        }
-
-    }
-    if (moves === 0 && openCards.length === 1) {
-        timer = setInterval(
-            () => timerFunction(timer),
-            1000
-        );
-     }
-
-}
-
+// -------------------------------------------STARS-------------------------------------------//
 // to remove a star from the score panel
 
 function removeStar(starArray) {
 	starArray.pop().remove();
 }
 
-// condition to rome stars based on moves numbers
+// condition to remove stars based on moves numbers
 
 function updateStars(starArray, moves){
     if (moves === 10 || moves === 20 || moves === 30) {
@@ -177,14 +101,7 @@ function updateStars(starArray, moves){
     }
 }
 
-// add opened cards to a cards array
-
-function addToOpenCardsArray(openCards, card){
-    if (card.classList.contains('open') && openCards.length < 2) {
-        openCards.push(card);
-    }
-}
-
+// -------------------------------------------MOVES--------------------------------------------//
 // update the score panel with the moves, where 2 clicks is one move
 
 function applyMovesToHTML(moves){
@@ -210,6 +127,94 @@ function increaseCounter(event){
 
 }
 
+// -------------------------------------------TIMER---------------------------------------------//
+ // Timer function adapted from https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
+
+ function pad (val) { 
+    return val > 9 ? val : "0" + val; 
+}
+ function timerFunction(timer){
+    seconds.innerHTML = pad(++sec % 60);
+    minutes.innerHTML = pad(parseInt(sec / 60, 10));
+}
+
+// -------------------------------------------MATCHES-------------------------------------------//
+// If all the cards are matched, call the modal
+function won(matchedCards) {
+    if (matchedCards.length === 16) {
+        clearInterval(timer);
+        toggleModal(moves,starArray);
+}
+}
+
+// add classes to matched cards
+function addMatchClass(card1, card2){
+    card1.children[0].classList.add('paired');
+    card2.children[0].classList.add('paired');
+    card1.classList.add('match');
+    card2.classList.add('match');
+
+}
+
+// add pairs to matched array and call function to add classes
+
+function addToMatched(matchedCards, card1,card2) {
+    matchedCards.push(card1);
+    matchedCards.push(card2);
+    addMatchClass(card1, card2);
+
+    won(matchedCards);
+}
+
+// remove and hide cards
+
+function removeAndHide(card1, card2){
+    card1.classList.remove('open');
+    card2.classList.remove('open');
+    openCards = [];
+}
+
+// -------------------------------------------COMPARE CARDS-------------------------------------------//
+
+function compareCards(openCards) {
+
+    const card1 = openCards[0];
+    const card2 = openCards[1];
+
+        // when they match
+    if (openCards.length === 2){
+        if(card1.innerHTML === card2.innerHTML){
+            addToMatched(matchedCards, card1, card2);
+            removeAndHide(card1, card2);
+
+        }
+        // when they don't match
+        else {
+            setTimeout(
+                () => removeAndHide(card1, card2),
+                1200
+            );
+        }
+
+    } 
+        //start timer
+    if (moves === 0 && openCards.length === 1) {
+        timer = setInterval(
+            () => timerFunction(timer),
+            1000
+        );
+     }
+
+}
+// -------------------------------------------CLICK AND OPEN--------------------------------------//
+// add opened cards to a cards array
+
+function addToOpenCardsArray(openCards, card){
+    if (card.classList.contains('open') && openCards.length < 2) {
+        openCards.push(card);
+    }
+}
+
 // function called when a click on a card happens: increase the moves counter, change class of opened cards and call compare cards function
 
 function flip(event) {
@@ -232,7 +237,7 @@ for (card of cards) {
 
 }
 
-// to restart the game, refresh the entire page
+// -------------------------------------------RESTART--------------------------------------//
 
 function restartGame() {
     location.reload();
